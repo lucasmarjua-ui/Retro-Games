dd560d4895c93ecaa25cee16d7e03b205e0e53d5
lucasmarjua-ui <lucas.mar.jua@gmail.com>
2025-11-06 17:24:53 +0100

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const supabase = createClient(
-  "https://TU-PROJECT-ID.supabase.co",
-  "TU-API-KEY-PUBLICA"
+  "https://https://gowgclioxsdmgugmydmv.supabase.co.supabase.co",
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdvd2djbGlveHNkbWd1Z215ZG12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0MzAwMjUsImV4cCI6MjA3ODAwNjAyNX0.tHXcV_Ks351TZBdfTCQbo0QQxumi8QUFlJLP8Vu8H2Y
 );
